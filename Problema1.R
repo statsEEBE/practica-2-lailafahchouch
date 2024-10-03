2b55720dfbd1b79c554f4ef4f691d31940bf0c50
laila.fahchouch <laila.fahchouch@auleseebe.upc.edu>
2024-10-03 11:29:19 +0200

    Problema 1 respostes

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,10 @@
 #Codigo para problema 1
 
+#pregunta 1--> coeficint de determinació tenim que mirarl el multiple R-squared
+
+#Pregunta 2--> fem l'arrel de a pregunta 1
+
+#Pregunta 3--> ens estan preguntat la pendent(variació de y per el increment de 1 unitat de x)
+# aixó es simplement es el pendent del primer model
+
+#Pregunta 4--> tenim que substituir m*647.1+(intercept)
